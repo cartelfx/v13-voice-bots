6af3bf95b9ac0bce3bb7ebb1ab0a35a503b7440b
cartel <119452317+cartelfx@users.noreply.github.com>
2024-01-27 21:54:55 +0400

    Update Voice.Client.js

--- Voice.Client.js
+++ Voice.Client.js
@@ -1,6 +1,6 @@
 ﻿const { Client,  VoiceChannel, GuildMember, PermissionFlagsBits, Guild } = require('discord.js');
 
-let sys = require('../Ayarlar/welcome.json');
+let sys = require('./welcome.json');
 
 const {
     createAudioPlayer,
